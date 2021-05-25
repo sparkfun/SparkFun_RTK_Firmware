26a85c9f3b014a6b7d26810a14555a0b2e4e9346
Nathan Seidle <nathan@sparkfun.com>
2021-05-25 15:54:43 -0600

    Fix for issue 21
    Nice fix. Thanks @Darmann2.

--- Firmware/RTK_Surveyor/Base.ino
+++ Firmware/RTK_Surveyor/Base.ino
@@ -119,16 +119,16 @@ bool startFixedBase()
   {
     //Break ECEF into main and high precision parts
     //The type casting should not effect rounding of original double cast coordinate
-    long majorEcefX = settings.fixedEcefX * 100;
-    long minorEcefX = ((settings.fixedEcefX * 100.0) - majorEcefX) * 100.0;
-    long majorEcefY = settings.fixedEcefY * 100;
-    long minorEcefY = ((settings.fixedEcefY * 100.0) - majorEcefY) * 100.0;
-    long majorEcefZ = settings.fixedEcefZ * 100;
-    long minorEcefZ = ((settings.fixedEcefZ * 100.0) - majorEcefZ) * 100.0;
+    long majorEcefX = floor((settings.fixedEcefX * 100.0) + 0.5);
+    long minorEcefX = floor((((settings.fixedEcefX * 100.0) - majorEcefX) * 100.0) + 0.5);
+    long majorEcefY = floor((settings.fixedEcefY * 100) + 0.5);
+    long minorEcefY = floor((((settings.fixedEcefY * 100.0) - majorEcefY) * 100.0) + 0.5);
+    long majorEcefZ = floor((settings.fixedEcefZ * 100) + 0.5);
+    long minorEcefZ = floor((((settings.fixedEcefZ * 100.0) - majorEcefZ) * 100.0) + 0.5);
 
-    //    Serial.printf("fixedEcefY (should be -4716808.5807): %0.04f\n\r", settings.fixedEcefY);
-    //    Serial.printf("major (should be -471680858): %d\n\r", majorEcefY);
-    //    Serial.printf("minor (should be -7): %d\n\r", minorEcefY);
+//    Serial.printf("fixedEcefY (should be -4716808.5807): %0.04f\n\r", settings.fixedEcefY);
+//    Serial.printf("major (should be -471680858): %ld\n\r", majorEcefY);
+//    Serial.printf("minor (should be -7): %ld\n\r", minorEcefY);
 
     //Units are cm with a high precision extension so -1234.5678 should be called: (-123456, -78)
     //-1280208.308,-4716803.847,4086665.811 is SparkFun HQ so...

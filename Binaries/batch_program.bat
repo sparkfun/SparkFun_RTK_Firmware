0312fb68090dd7c5e720751dd16e25e4fdc8d54a
Nathan Seidle <nathan@sparkfun.com>
2021-06-07 14:15:08 -0600

    Update batch file to use new binary

--- Binaries/batch_program.bat
+++ Binaries/batch_program.bat
@@ -9,7 +9,7 @@ if [%1]==[] goto usage
 @echo -
 @echo Programming binary: %1 on %2
 
-rem @esptool.exe --chip esp32 --port COM6 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0 RTK_Surveyor_Firmware_v12_combined.bin
+rem @esptool.exe --chip esp32 --port COM6 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0 RTK_Surveyor_Firmware_v13_combined.bin
 @esptool.exe --chip esp32 --port %2 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0 %1
 
 @echo Done programming! Ready for next board.

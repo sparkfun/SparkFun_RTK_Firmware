aeffe71c75f97f250d202e6ad44a51b3c1638b06
Nathan Seidle <nathan@sparkfun.com>
2021-09-16 14:33:09 -0600

    Remove numbers from compiler guards

--- Firmware/RTK_Surveyor/RTK_Surveyor.ino
+++ Firmware/RTK_Surveyor/RTK_Surveyor.ino
@@ -41,8 +41,8 @@
 const int FIRMWARE_VERSION_MAJOR = 1;
 const int FIRMWARE_VERSION_MINOR = 5;
 
-#define COMPILE_WIFI 1 //Comment out to remove all WiFi functionality
-#define COMPILE_BT 1 //Comment out to disable all Bluetooth
+#define COMPILE_WIFI //Comment out to remove all WiFi functionality
+#define COMPILE_BT //Comment out to disable all Bluetooth
 //#define ENABLE_DEVELOPER //Uncomment this line to enable special developer modes (don't check power button at startup)
 
 //Define the RTK board identifier:

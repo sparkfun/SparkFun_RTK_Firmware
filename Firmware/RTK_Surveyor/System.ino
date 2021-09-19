bf0fc9cd62d57ee3437c64567a945d66a45aaf63
Nathan Seidle <nathan@sparkfun.com>
2021-09-18 20:08:45 -0600

    Disable NMEA (and RTCM3) over I2C to decrease reset occurance.
    NMEA at 4Hz including GxGSV increases the number of bytes to the ESP32. The ZED-F9x is known to have issues creating bad I2C traffic. These bad transmissions lead to possible ESP32 un-recoverable errors and resets. By decreasing I2C traffic we reduce the possibility of a reset.

--- Firmware/RTK_Surveyor/System.ino
+++ Firmware/RTK_Surveyor/System.ino
@@ -177,44 +177,42 @@ bool configureUbloxModule()
   //but the phone can also provide RTCM data and a user may want to configure the ZED over Bluetooth.
   //So let's be sure to enable UBX+NMEA+RTCM on the input
   getPortSettings(COM_PORT_UART1); //Load the settingPayload with this port's settings
-  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3) || settingPayload[INPUT_SETTING] != (COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3))
-  {
+  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3))
     response &= i2cGNSS.setPortOutput(COM_PORT_UART1, COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3); //Set the UART1 to output UBX+NMEA+RTCM
+
+  if (settingPayload[INPUT_SETTING] != (COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3))
     response &= i2cGNSS.setPortInput(COM_PORT_UART1, COM_TYPE_NMEA | COM_TYPE_UBX | COM_TYPE_RTCM3); //Set the UART1 to input UBX+NMEA+RTCM
-  }
 
   //Disable SPI port - This is just to remove some overhead by ZED
   getPortSettings(COM_PORT_SPI); //Load the settingPayload with this port's settings
-  if (settingPayload[OUTPUT_SETTING] != 0 || settingPayload[INPUT_SETTING] != 0)
-  {
+  if (settingPayload[OUTPUT_SETTING] != 0)
     response &= i2cGNSS.setPortOutput(COM_PORT_SPI, 0); //Disable all protocols
+  if (settingPayload[INPUT_SETTING] != 0)
     response &= i2cGNSS.setPortInput(COM_PORT_SPI, 0); //Disable all protocols
-  }
 
   getPortSettings(COM_PORT_UART2); //Load the settingPayload with this port's settings
-  if (settingPayload[OUTPUT_SETTING] != COM_TYPE_RTCM3 || settingPayload[INPUT_SETTING] != COM_TYPE_RTCM3)
-  {
+  if (settingPayload[OUTPUT_SETTING] != COM_TYPE_RTCM3)
     response &= i2cGNSS.setPortOutput(COM_PORT_UART2, COM_TYPE_RTCM3); //Set the UART2 to output RTCM (in case this device goes into base mode)
+  if (settingPayload[INPUT_SETTING] != COM_TYPE_RTCM3)
     response &= i2cGNSS.setPortInput(COM_PORT_UART2, COM_TYPE_RTCM3); //Set the UART2 to input RTCM
-  }
 
   //Turn on RTCM over I2C port so that we can harvest RTCM over I2C and send out over WiFi
   //This is easier than parsing over UART because the library handles the frame detection
   getPortSettings(COM_PORT_I2C); //Load the settingPayload with this port's settings
-  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_UBX | COM_TYPE_RTCM3) || settingPayload[INPUT_SETTING] != COM_TYPE_UBX)
-  {
-    response &= i2cGNSS.setPortOutput(COM_PORT_I2C, COM_TYPE_UBX | COM_TYPE_RTCM3); //Set the I2C port to output UBX (config), NMEA (logging), and RTCM3 (casting)
+  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_UBX))
+    response &= i2cGNSS.setPortOutput(COM_PORT_I2C, COM_TYPE_UBX); //Set the I2C port to output UBX (config)
+  //response &= i2cGNSS.setPortOutput(COM_PORT_I2C, COM_TYPE_UBX | COM_TYPE_RTCM3); //Not a valid state. Goes to UBX+I2C+ RTCM3 - Set the I2C port to output UBX (config), and RTCM3 (casting)
+
+  if (settingPayload[INPUT_SETTING] != COM_TYPE_UBX)
     response &= i2cGNSS.setPortInput(COM_PORT_I2C, COM_TYPE_UBX); //Set the I2C port to input UBX only
-  }
 
   //The USB port on the ZED may be used for RTCM to/from the computer (as an NTRIP caster or client)
   //So let's be sure all protocols are on for the USB port
   getPortSettings(COM_PORT_USB); //Load the settingPayload with this port's settings
-  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3) || settingPayload[INPUT_SETTING] != (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3))
-  {
+  if (settingPayload[OUTPUT_SETTING] != (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3))
     response &= i2cGNSS.setPortOutput(COM_PORT_USB, (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3)); //Set the USB port to everything
+  if (settingPayload[INPUT_SETTING] != (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3))
     response &= i2cGNSS.setPortInput(COM_PORT_USB, (COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3)); //Set the USB port to everything
-  }
 
   response &= configureConstellations(); //Enable the constellations the user has set
 

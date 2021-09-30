296ca5e526e87c6542e22682a6b0512be986c5dc
Nathan Seidle <nathan@sparkfun.com>
2021-09-30 15:54:15 -0600

    Change max log time default to 24 hours

--- Firmware/RTK_Surveyor/settings.h
+++ Firmware/RTK_Surveyor/settings.h
@@ -142,7 +142,7 @@ struct struct_settings {
   bool printDebugMessages = false;
   bool enableSD = true;
   bool enableDisplay = true;
-  int maxLogTime_minutes = 60 * 10; //Default to 10 hours
+  int maxLogTime_minutes = 60 * 24; //Default to 24 hours
   int observationSeconds = 60; //Default survey in time of 60 seconds
   float observationPositionAccuracy = 5.0; //Default survey in pos accy of 5m
   bool fixedBase = false; //Use survey-in by default

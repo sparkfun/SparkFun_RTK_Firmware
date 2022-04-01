a92eb28c19614537fee14233cf3f2940b90a137a
Nathan Seidle <nathan@sparkfun.com>
2022-04-01 14:43:06 -0600

    Update RTK_Surveyor.ino

--- Firmware/RTK_Surveyor/RTK_Surveyor.ino
+++ Firmware/RTK_Surveyor/RTK_Surveyor.ino
@@ -48,7 +48,7 @@
 */
 
 const int FIRMWARE_VERSION_MAJOR = 1;
-const int FIRMWARE_VERSION_MINOR = 11;
+const int FIRMWARE_VERSION_MINOR = 12;
 
 #define COMPILE_WIFI //Comment out to remove all WiFi functionality
 #define COMPILE_BT //Comment out to disable all Bluetooth

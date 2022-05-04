29a6527b2b5d5f2a242139aaec40dbc463ece009
Lee Leahy <lpleahyjr@gmail.com>
2022-05-03 18:05:10 -1000

    WebServer: Break out print routines, beginSD

--- Firmware/Test Sketches/WebServer/WebServer.ino
+++ Firmware/Test Sketches/WebServer/WebServer.ino
@@ -2,9 +2,8 @@
   Web server test program
 */
 
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <SdCardServer.h>
+#include <ESPAsyncWebServer.h>  //Get from: https://github.com/me-no-dev/ESPAsyncWebServer
+#include <SdCardServer.h>       //Get v1.0 from: https://github.com/LeeLeahy2/SdCardServer
 #include <SPI.h>
 #include <pgmspace.h>
 #include <WiFi.h>
@@ -18,218 +17,17 @@ int keyIndex = 0;
 char password[1024];          // WiFi network password
 char ssid[1024];              // WiFi network name
 
-const int pin_microSD_CS = 25;
-
-typedef struct struct_settings {
-  bool enableSD = true;
-  uint16_t spiFrequency = 16; //By default, use 16MHz SPI
-} Settings;
-
 typedef struct struct_online {
   bool microSD = false;
 } Online;
 
-const TickType_t fatSemaphore_shortWait_ms = 10 / portTICK_PERIOD_MS;
-const TickType_t fatSemaphore_longWait_ms = 200 / portTICK_PERIOD_MS;
 Online online;
 SdFat sd;
 int sdCardMounted;
 AsyncWebServer server(80);
-Settings settings;
 int status = WL_IDLE_STATUS;  // the Wifi radio's status
 int wifiBeginCalled;
 int wifiConnected;
-SemaphoreHandle_t xFATSemaphore;
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void beginSD()
-{
-  pinMode(pin_microSD_CS, OUTPUT);
-  digitalWrite(pin_microSD_CS, HIGH); //Be sure SD is deselected
-
-  if (settings.enableSD == true)
-  {
-    //Do a quick test to see if a card is present
-    int tries = 0;
-    int maxTries = 5;
-    while (tries < maxTries)
-    {
-      if (sdPresent() == true) break;
-      log_d("SD present failed. Trying again %d out of %d", tries + 1, maxTries);
-
-      //Max power up time is 250ms: https://www.kingston.com/datasheets/SDCIT-specsheet-64gb_en.pdf
-      //Max current is 200mA average across 1s, peak 300mA
-      delay(10);
-      tries++;
-    }
-    if (tries == maxTries) return;
-
-    //If an SD card is present, allow SdFat to take over
-    log_d("SD card detected");
-
-    if (settings.spiFrequency > 16)
-    {
-      Serial.println("Error: SPI Frequency out of range. Default to 16MHz");
-      settings.spiFrequency = 16;
-    }
-
-    if (sd.begin(SdSpiConfig(pin_microSD_CS, SHARED_SPI, SD_SCK_MHZ(settings.spiFrequency))) == false)
-    {
-      tries = 0;
-      maxTries = 1;
-      for ( ; tries < maxTries ; tries++)
-      {
-        log_d("SD init failed. Trying again %d out of %d", tries + 1, maxTries);
-
-        delay(250); //Give SD more time to power up, then try again
-        if (sd.begin(SdSpiConfig(pin_microSD_CS, SHARED_SPI, SD_SCK_MHZ(settings.spiFrequency))) == true) break;
-      }
-
-      if (tries == maxTries)
-      {
-        Serial.println(F("SD init failed. Is card present? Formatted?"));
-        digitalWrite(pin_microSD_CS, HIGH); //Be sure SD is deselected
-        online.microSD = false;
-        return;
-      }
-    }
-
-    //Change to root directory. All new file creation will be in root.
-    if (sd.chdir() == false)
-    {
-      Serial.println(F("SD change directory failed"));
-      online.microSD = false;
-      return;
-    }
-
-    //Setup FAT file access semaphore
-    if (xFATSemaphore == NULL)
-    {
-      xFATSemaphore = xSemaphoreCreateMutex();
-      if (xFATSemaphore != NULL)
-        xSemaphoreGive(xFATSemaphore);  //Make the file system available for use
-    }
-
-    if (createTestFile() == false)
-    {
-      Serial.println(F("Failed to create test file. Format SD card with 'SD Card Formatter'."));
-      displaySDFail(5000);
-      online.microSD = false;
-      return;
-    }
-
-    online.microSD = true;
-
-    Serial.println(F("microSD online"));
-  }
-  else
-  {
-    online.microSD = false;
-  }
-}
-
-//Create a test file in file structure to make sure we can
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-bool createTestFile()
-{
-  SdFile testFile;
-  char testFileName[40] = "testfile.txt";
-
-  if (xFATSemaphore == NULL)
-  {
-    log_d("xFATSemaphore is Null");
-    return (false);
-  }
-
-  //Attempt to write to file system. This avoids collisions with file writing from other functions like recordSystemSettingsToFile() and F9PSerialReadTask()
-  if (xSemaphoreTake(xFATSemaphore, fatSemaphore_shortWait_ms) == pdPASS)
-  {
-    if (testFile.open(testFileName, O_CREAT | O_APPEND | O_WRITE) == true)
-    {
-      testFile.close();
-
-      if (sd.exists(testFileName))
-        sd.remove(testFileName);
-      xSemaphoreGive(xFATSemaphore);
-      return (true);
-    }
-    xSemaphoreGive(xFATSemaphore);
-  }
-
-  return (false);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayIpAddress(IPAddress ip) {
-
-    // Display the IP address
-    Serial.println(ip);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displaySDFail(uint16_t displayTime)
-{
-  Serial.println ("SD card failed to initialize!");
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayWiFiGatewayIp() {
-
-    // Display the subnet mask
-    Serial.print ("Gateway: ");
-    IPAddress ip = WiFi.gatewayIP();
-    displayIpAddress(ip);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayWiFiIpAddress() {
-
-    // Display the IP address
-    Serial.print ("IP Address: ");
-    IPAddress ip = WiFi.localIP();
-    displayIpAddress(ip);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayWiFiMacAddress() {
-    // Display the MAC address
-    byte mac[6];
-    WiFi.macAddress(mac);
-    Serial.print("MAC address: ");
-    Serial.print(mac[5], HEX);
-    Serial.print(":");
-    Serial.print(mac[4], HEX);
-    Serial.print(":");
-    Serial.print(mac[3], HEX);
-    Serial.print(":");
-    Serial.print(mac[2], HEX);
-    Serial.print(":");
-    Serial.print(mac[1], HEX);
-    Serial.print(":");
-    Serial.println(mac[0], HEX);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayWiFiNetwork() {
-
-    // Display the SSID
-    Serial.print("SSID: ");
-    Serial.println(WiFi.SSID());
-
-    // Display the receive signal strength
-    long rssi = WiFi.RSSI();
-    Serial.print("Signal strength (RSSI):");
-    Serial.println(rssi);
-}
-
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-void displayWiFiSubnetMask() {
-
-    // Display the subnet mask
-    Serial.print ("Subnet Mask: ");
-    IPAddress ip = WiFi.subnetMask();
-    displayIpAddress(ip);
-}
 
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 int mountSdCard(void) {
@@ -326,10 +124,10 @@ void loop() {
             Serial.println("WiFi Connected");
 
             // Display the WiFi info
-            displayWiFiNetwork();
-            displayWiFiIpAddress();
-            displayWiFiSubnetMask();
-            displayWiFiGatewayIp();
+            printWiFiNetwork();
+            printWiFiIpAddress();
+            printWiFiSubnetMask();
+            printWiFiGatewayIp();
 
             // index.html
             sdCardServer.sdCardWebSite(&server);

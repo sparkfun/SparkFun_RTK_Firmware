3bf28ad9dff962fd83381ef25e38ec44df4bdd3b
Nathan Seidle <nathan@sparkfun.com>
2022-05-13 13:23:05 -0600

    Add test screen entry to debug menu

--- Firmware/RTK_Surveyor/menuSystem.ino
+++ Firmware/RTK_Surveyor/menuSystem.ino
@@ -48,8 +48,8 @@ void menuSystem()
       Serial.print(F("L-Band: Online - "));
       if (online.lbandCorrections == true) Serial.print(F("Keys Good"));
       else Serial.print(F("No Keys"));
-      
-      if(lbandCorrectionsReceived == true) Serial.print(F(" / Corrections Received"));
+
+      if (lbandCorrectionsReceived == true) Serial.print(F(" / Corrections Received"));
       else Serial.print(F(" / Corrections Received Failed"));
 
       Serial.printf(" / Eb/N0[dB] (>9 is good): %0.2f", lBandEBNO);
@@ -70,11 +70,9 @@ void menuSystem()
       if (zedUartPassed == false)
       {
         stopUART2Tasks(); //Stop absoring ZED serial via task
-        delay(250); //Wait for tasks to abort
 
-        //Clear out buffer before starting
-        while (serialGNSS.available()) serialGNSS.read();
-        serialGNSS.flush();
+        i2cGNSS.setSerialRate(460800, COM_PORT_UART1); //Defaults to 460800 to maximize message output support
+        serialGNSS.begin(460800); //UART2 on pins 16/17 for SPP. The ZED-F9P will be configured to output NMEA over its UART1 at the same rate.
 
         SFE_UBLOX_GNSS myGNSS;
         if (myGNSS.begin(serialGNSS) == true) //begin() attempts 3 connections
@@ -84,6 +82,9 @@ void menuSystem()
         }
         else
           Serial.print(F("Offline"));
+
+        i2cGNSS.setSerialRate(settings.dataPortBaud, COM_PORT_UART1); //Defaults to 460800 to maximize message output support
+        serialGNSS.begin(settings.dataPortBaud); //UART2 on pins 16/17 for SPP. The ZED-F9P will be configured to output NMEA over its UART1 at the same rate.
 
         startUART2Tasks(); //Return to normal operation
       }
@@ -188,6 +189,8 @@ void menuDebug()
     Serial.print(F("9) GNSS Serial Timeout: "));
     Serial.println(settings.serialTimeoutGNSS);
 
+    Serial.println(F("t) Enter Test Screen"));
+
     Serial.println(F("e) Erase LittleFS"));
 
     Serial.println(F("r) Force system reset"));
@@ -287,6 +290,10 @@ void menuDebug()
     else if (incoming == 'r')
     {
       ESP.restart();
+    }
+    else if (incoming == 't')
+    {
+      requestChangeState(STATE_TEST); //We'll enter test mode once exiting all serial menus
     }
     else if (incoming == 'x')
       break;

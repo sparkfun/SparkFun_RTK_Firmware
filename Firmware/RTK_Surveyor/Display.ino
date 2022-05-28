cc807df4781c5d7cc13c8182833c3454aec0eecc
Lee Leahy <lpleahyjr@gmail.com>
2022-05-28 11:49:14 -1000

    Move beginDisplay into Display.ino

--- Firmware/RTK_Surveyor/Display.ino
+++ Firmware/RTK_Surveyor/Display.ino
@@ -31,6 +31,30 @@
 
 static uint32_t icons;
 
+void beginDisplay()
+{
+  blinking_icons = 0;
+  if (oled.begin() == true)
+  {
+    online.display = true;
+
+    Serial.println(F("Display started"));
+    displaySplash();
+    splashStart = millis();
+  }
+  else
+  {
+    if (productVariant == RTK_SURVEYOR)
+    {
+      Serial.println(F("Display not detected"));
+    }
+    else if (productVariant == RTK_EXPRESS || productVariant == RTK_EXPRESS_PLUS || productVariant == RTK_FACET || productVariant == RTK_FACET_LBAND)
+    {
+      Serial.println(F("Display Error: Not detected."));
+    }
+  }
+}
+
 //Given the system state, display the appropriate information
 void updateDisplay()
 {

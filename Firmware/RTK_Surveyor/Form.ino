ecb81e9c1ba9f252eb448025fed2bd82bcc46763
Lee Leahy <lpleahyjr@gmail.com>
2022-05-29 11:07:51 -1000

    Move calls to WiFi.status into WiFi.ino

--- Firmware/RTK_Surveyor/Form.ino
+++ Firmware/RTK_Surveyor/Form.ino
@@ -60,7 +60,7 @@ void startWebServer()
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
+  while (wifiGetStatus() != WL_CONNECTED)
   {
     Serial.print(".");
     delay(500);
--- Firmware/RTK_Surveyor/menuPP.ino
+++ Firmware/RTK_Surveyor/menuPP.ino
@@ -73,14 +73,14 @@ void menuPointPerfect()
       startWiFi(settings.home_wifiSSID, settings.home_wifiPW);
 
       unsigned long startTime = millis();
-      while (WiFi.status() != WL_CONNECTED)
+      while (wifiGetStatus() != WL_CONNECTED)
       {
         delay(500);
         Serial.print(".");
         if (millis() - startTime > 8000) break; //Give up after 8 seconds
       }
 
-      if (WiFi.status() == WL_CONNECTED)
+      if (wifiGetStatus() == WL_CONNECTED)
       {
 
         Serial.println();

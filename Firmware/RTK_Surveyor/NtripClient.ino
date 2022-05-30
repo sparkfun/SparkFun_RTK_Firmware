5e4f4d49ebbf61a70a755e3e8805be2a249269e6
Lee Leahy <lpleahyjr@gmail.com>
2022-05-30 06:58:51 -1000

    Add WiFi and NTRIP Client state diagrams

--- Firmware/RTK_Surveyor/NtripClient.ino
+++ Firmware/RTK_Surveyor/NtripClient.ino
@@ -1,3 +1,36 @@
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+NTRIP Client States:
+    NTRIP_CLIENT_OFF: Using Bluetooth or NTRIP server
+    NTRIP_CLIENT_ON: WIFI_ON state
+    NTRIP_CLIENT_WIFI_CONNECTING: Connecting to WiFi access point
+    NTRIP_CLIENT_WIFI_CONNECTED: WiFi connected to an access point
+    NTRIP_CLIENT_CONNECTING: Attempting a connection to the NTRIP caster
+    NTRIP_CLIENT_CONNECTED: Connected to the NTRIP caster
+
+                               NTRIP_CLIENT_OFF
+                                    |   ^
+                   ntripClientStart |   | ntripClientStop
+                                    v   |
+                               NTRIP_CLIENT_ON
+                                    |   ^
+                                    |   |
+                                    v   |
+                         NTRIP_CLIENT_WIFI_CONNECTING
+                                    |   ^
+                                    |   |
+                                    v   |
+                         NTRIP_CLIENT_WIFI_CONNECTED
+                                    |   ^
+                                    |   |
+                                    v   |
+                           NTRIP_CLIENT_CONNECTING
+                                    |   ^
+                                    |   |
+                                    v   |
+                            NTRIP_CLIENT_CONNECTED
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
 //----------------------------------------
 // Constants
 //----------------------------------------

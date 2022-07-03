48a2339f4d3e09dffd96d7a0fa30bf79124f842d
Lee Leahy <lpleahyjr@gmail.com>
2022-07-03 10:45:23 -1000

    Increase the number of profiles to 8

--- Firmware/RTK_Surveyor/RTK_Surveyor.ino
+++ Firmware/RTK_Surveyor/RTK_Surveyor.ino
@@ -85,7 +85,7 @@ int pin_radio_rts;
 
 const char *rtkProfileSettings = "SFERTK"; //Holds the profileNumber
 const char *rtkSettings[] = {"SFERTK_0", "SFERTK_1", "SFERTK_2", "SFERTK_3"}; //User profiles
-#define MAX_PROFILE_COUNT 4
+#define MAX_PROFILE_COUNT 8
 uint8_t activeProfiles = 0; //Bit vector indicating which profiles are active
 uint8_t displayProfile; //Range: 0 - (MAX_PROFILE_COUNT - 1)
 uint8_t profileNumber = MAX_PROFILE_COUNT; //profileNumber gets set once at boot to save loading time

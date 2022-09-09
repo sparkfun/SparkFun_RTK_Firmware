2d74e27087dd6a87bebab9a5c986b7afac1f645d
Nathan Seidle <nathan@sparkfun.com>
2022-09-09 10:01:13 -0600

    Fix timer for initial pvt wait

--- Firmware/RTK_Surveyor/System.ino
+++ Firmware/RTK_Surveyor/System.ino
@@ -6,9 +6,10 @@ bool configureUbloxModule()
   if (online.gnss == false) return (false);
 
   boolean response = true;
-  int maxWait = 2000;
 
   //Wait for initial report from module
+  int maxWait = 2000;
+  startTime = millis();
   while (pvtUpdated == false)
   {
     i2cGNSS.checkUblox(); //Regularly poll to get latest data and any RTCM

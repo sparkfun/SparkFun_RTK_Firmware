8bdd91735d72195957b98f54f8a7dcb96c25eb33
Nathan Seidle <nathan@sparkfun.com>
2022-09-15 08:48:35 -0600

    Change gateway to match IP during AP config

--- Firmware/RTK_Surveyor/WiFi.ino
+++ Firmware/RTK_Surveyor/WiFi.ino
@@ -180,7 +180,7 @@ void wifiStartAP()
 #endif
 
   IPAddress local_IP(192, 168, 4, 1);
-  IPAddress gateway(192, 168, 1, 1);
+  IPAddress gateway(192, 168, 4, 1);
   IPAddress subnet(255, 255, 255, 0);
 
   WiFi.softAPConfig(local_IP, gateway, subnet);

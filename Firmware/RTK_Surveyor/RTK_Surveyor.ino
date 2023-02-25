3ce816adad0c04f6ab239a9c3275d1def3992f5f
Nathan Seidle <nathan@sparkfun.com>
2023-02-24 21:21:26 -0700

    Enable BT and Dev on RC

--- Firmware/RTK_Surveyor/RTK_Surveyor.ino
+++ Firmware/RTK_Surveyor/RTK_Surveyor.ino
@@ -28,10 +28,10 @@
 #define COMPILE_WIFI //Comment out to remove WiFi functionality
 #define COMPILE_AP //Requires WiFi. Comment out to remove Access Point functionality
 #define COMPILE_ESPNOW //Requires WiFi. Comment out to remove ESP-Now functionality.
-//#define COMPILE_BT //Comment out to remove Bluetooth functionality
+#define COMPILE_BT //Comment out to remove Bluetooth functionality
 #define COMPILE_L_BAND //Comment out to remove L-Band functionality
 #define COMPILE_SD_MMC // Comment out to remove REFERENCE_STATION microSD SD_MMC support
-//#define ENABLE_DEVELOPER //Uncomment this line to enable special developer modes (don't check power button at startup)
+#define ENABLE_DEVELOPER //Uncomment this line to enable special developer modes (don't check power button at startup)
 
 //Define the RTK board identifier:
 //  This is an int which is unique to this variant of the RTK Surveyor hardware which allows us

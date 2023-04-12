a96bc6a409c6cb9ecdbb25817473834dddef4035
nseidle <nathan@sparkfun.com>
2023-04-12 15:36:27 -0600

    Update library note

--- Firmware/RTK_Surveyor/RTK_Surveyor.ino
+++ Firmware/RTK_Surveyor/RTK_Surveyor.ino
@@ -183,7 +183,7 @@ char logFileName[sizeof("SFE_Facet_L-Band_230101_120101.ubx_plusExtraSpace")] = 
 #include <HTTPClient.h> //Built-in. Needed for ThingStream API for ZTP
 #include <ArduinoJson.h> //http://librarymanager/All#Arduino_JSON_messagepack v6.19.4
 #include <WiFiClientSecure.h> //Built-in.
-#include <PubSubClient.h> //http://librarymanager/All#PubSubClient_MQTT_Lightweight v2.8.0 Used for MQTT obtaining of keys
+#include <PubSubClient.h> //http://librarymanager/All#PubSubClient_MQTT_Lightweight by Nick O'Leary v2.8.0 Used for MQTT obtaining of keys
 #include "ESP32OTAPull.h" //http://librarymanager/All#ESP-OTA-Pull Used for getting latest firmware OTA
 
 #include "esp_wifi.h" //Needed for esp_wifi_set_protocol()

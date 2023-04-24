f043012853bddd4750c7f113ce28209c81acb8a1
nseidle <nathan@sparkfun.com>
2023-04-24 14:14:34 -0600

    Print date in log
    Fix for #440

--- Firmware/RTK_Surveyor/settings.h
+++ Firmware/RTK_Surveyor/settings.h
@@ -131,6 +131,7 @@ typedef enum
   CUSTOM_NMEA_TYPE_LOGTEST_STATUS,
   CUSTOM_NMEA_TYPE_DEVICE_BT_ID,
   CUSTOM_NMEA_TYPE_PARSER_STATS,
+  CUSTOM_NMEA_TYPE_CURRENT_DATE,
 } customNmeaType_e;
 
 //Freeze and blink LEDs if we hit a bad error

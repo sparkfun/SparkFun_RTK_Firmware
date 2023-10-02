fe0148a7d0e47e41d6814bdcafcba3c930b33cd7
Nathan Seidle <nathan@sparkfun.com>
2023-10-02 10:11:29 -0600

    Add discardPvtUdpServerBytes to developer.ino to enable WiFi guard

--- Firmware/RTK_Surveyor/Developer.ino
+++ Firmware/RTK_Surveyor/Developer.ino
@@ -112,6 +112,7 @@ void pvtServerUpdate() {}
 void pvtServerZeroTail() {}
 void pvtServerValidateTables() {}
 void discardPvtServerBytes(RING_BUFFER_OFFSET previousTail, RING_BUFFER_OFFSET newTail) {}
+void discardPvtUdpServerBytes(RING_BUFFER_OFFSET previousTail, RING_BUFFER_OFFSET newTail) {}
 
 //----------------------------------------
 // PVT UDP server
